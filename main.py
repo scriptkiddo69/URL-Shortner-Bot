482185129bee1013058487bc780649699d5275d5
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-12-02 13:18:55 +0530

    Update code

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
 from pyrogram import Client
 
-bot_token = os.environ["BOT_TOKEN"]
-api_id = int(os.environ["API_ID"])
-api_hash = os.environ["API_HASH"]
-plugins = dict(
-    root="plugins"
-)
+
+bot_token = os.environ.get("BOT_TOKEN")
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+plugins = dict(root="plugins")
 
 Bot = Client(
     "URL-Shortner-Bot",
